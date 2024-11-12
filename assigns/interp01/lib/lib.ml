c4a721fefd206628445080b4e2c8728ab1bb1a2a
99Niki <zhengliting378@gmail.com>
2024-11-12 12:23:21 -0500

    committing assign

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -1,46 +1,69 @@
 open Utils
+open My_parser
 
-let parse = My_parser.parse
+let parse (s:string):expr option=
+  parse(s)
 
+let replace_var x y =
+  let rec go = function
+    | Var z -> if z = y then Var x else Var z
+    | App (e1, e2) -> App (go e1, go e2)
+    | Fun (z, e) -> Fun (z, go e)
+    | Let (z, e1, e2) -> Let(z, go e1, go e2)
+    | Unit -> Unit
+    | True -> True
+    | False -> False
+    | Num m -> Num m
+    | Bop (op,e1,e2)-> Bop(op, go e1,go e2)
+    | If (e1,e2,e3)-> If (go e1, go e2,go e3)
+  in 
+  go
+  
 let rec subst (v:value) (x:string) (e:expr):expr =
+  let ve = 
+    match v with
+  | VNum n -> (Num n)
+  | VBool b -> (if b then True else False)
+  | VUnit -> Unit
+  | VFun (p,b) -> Fun (p,b)
+  in
   match e with
-  | Num n -> (Num n)
-  | Var y -> if y = x then 
-              (match v with
-                | VNum n -> Num n
-                | VBool b -> if b then True else False
-                | VUnit -> Unit
-                | VFun (v1, e1) -> Fun (v1, e1)
-                )
-              else Var y
-  | Unit -> Unit
-  | True -> True
-  | False -> False
-  | If (e1, e2, e3) -> If (subst v x e1, subst v x e2, subst v x e3)
-  | Let (y, e1, e2) ->
-      if y = x then Let (y, subst v x e1, e2)
-      else Let (y, subst v x e1, subst v x e2)
-  | Fun (x2, e2) ->
-      if x2 = x then Fun (x2, e2)
-      else Fun (x2, subst v x e2)
-  | App (e1, e2) -> App (subst v x e1, subst v x e2)
-  | Bop (op, e1, e2) -> Bop (op, subst v x e1, subst v x e2)
+  | Var y -> if y=x then ve else e
+  | App(e1,e2) -> App(subst v x e1, subst v x e2)
+  | Bop(op,e1,e2)-> Bop (op, subst v x e1, subst v x e2)
+  | If (c, e1,e2) -> If (subst v x c, subst v x e1, subst v x e2)
+  | Let (e1,e2,e3) -> 
+      (
+        if e1 = x then Let (e1,subst v x e2, e3)
+        else
+          let z = gensym () in
+        Let (z, subst v x e2, subst v x (replace_var z e1 e3))
+      )
+  | Fun (p,b) -> 
+    (
+      if p = x then e
+      else
+        let z = gensym() in
+        Fun (z,subst v x (replace_var z p b))
+        )
+  | _ -> e 
 
+let ( let* ) = Option.bind
 
 let rec eval (e:expr): (value,error)result=
   match e with
   | Num n -> Ok(VNum n)
   | Var v -> Error (UnknownVar v)
-  | Unit -> Ok VUnit
+  | Unit -> Ok (VUnit)
   | True -> Ok (VBool true)
   | False -> Ok (VBool false)
   | App (e1,e2) -> 
     (
       match eval e1 with
-      | Ok(VFun (x,e))->
+      | Ok(VFun (p,b))->
         (
           match eval e2 with
-          | Ok v -> eval (subst v x e)
+          | Ok v -> eval (subst v p b)
           | Error e -> Error e
         )
       | Ok _ -> Error InvalidApp
@@ -55,8 +78,10 @@ let rec eval (e:expr): (value,error)result=
           | Add -> Ok (VNum (n1 + n2))
           | Sub -> Ok (VNum (n1 - n2))
           | Mul -> Ok (VNum (n1 * n2))
-          | Div -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
-          | Mod -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
+          | Div -> (if n2 = 0 then Error DivByZero 
+                    else Ok (VNum (n1 / n2)))
+          | Mod -> (if n2 = 0 then Error DivByZero 
+                    else Ok (VNum (n1 mod n2)))
           | Lt -> Ok (VBool (n1 < n2))
           | Lte -> Ok (VBool (n1 <= n2))
           | Gt -> Ok (VBool (n1 > n2))
@@ -79,8 +104,9 @@ let rec eval (e:expr): (value,error)result=
     (match eval e1 with
      | Ok (VBool true) -> eval e2
      | Ok (VBool false) -> eval e3
-     | Ok _ -> Error InvalidIfCond
-     | Error e -> Error e)
+     | Error e -> Error e
+     | _ -> Error InvalidIfCond
+     )
   | Let (x, e1, e2) ->
       (match eval e1 with
       | Ok v1 -> eval (subst v1 x e2)
@@ -88,10 +114,10 @@ let rec eval (e:expr): (value,error)result=
   | Fun (x, e) -> Ok (VFun (x, e))
   
   
-
   let interp (s : string) : (value, error) result =
     match My_parser.parse s with
-    | None -> Error ParseFail
     | Some prog -> eval prog
+    | _ -> Error ParseFail
+    
   
   
